1c16608b7eaf7b202361d986c391fd3e810ad1e0
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-06-12 23:50:02 +0530

    still working on it

---
+++ UniversityDegree.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier : MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract UniversityDegree is ERC721URIStorage{
+
+    address owner;
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    constructor() ERC721("UniversityDegree","Degree"){
+        owner = msg.sender;
+    }
+
+    mapping(address => bool) public issuedDegrees;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function issueDegree(address to) external {
+        issuedDegrees[to]=true;
+    }
+
+    function claimDegree(string memory tokenURI) public returns(uint256){
+        require(issuedDegrees[msg.sender],"Degree is not issued");
+        
+    }
+  }
+
+
